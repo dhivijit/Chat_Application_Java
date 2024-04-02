c75a568a211f7cd0bc58511acf982ba805aab47f
Dhivijit Koppuravuri <68179868+dhivijit@users.noreply.github.com>
2024-04-02 17:48:41 +0530

    Update ChatMessage.java

--- Chat_Application_server/src/main/java/application/ChatMessage.java
+++ Chat_Application_server/src/main/java/application/ChatMessage.java
@@ -1,16 +1,19 @@
 package application;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 public class ChatMessage implements Serializable {
     private String sender;
     private String message;
     private String chatroomID;
+    private Timestamp timestamp;
 
-    public ChatMessage(String sender, String message, String chatroomID) {
+    public ChatMessage(String sender, String message, String chatroomID, Timestamp timestamp) {
         this.sender = sender;
         this.message = message;
         this.chatroomID=chatroomID;
+        this.timestamp = timestamp;
     }
 
     public String getSender() {
@@ -23,5 +26,9 @@ public class ChatMessage implements Serializable {
 
     public String getChatroomID() {
         return chatroomID;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 }
